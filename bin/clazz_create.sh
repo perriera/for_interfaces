474de589d82d7b427c84b13a86caf799b7d52e5e
Your Name <you@example.com>
2023-11-15 09:37:27 -0800

    Added v0.22.0

---
+++ bin/clazz_create.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+#
+# @brief The MIT License (MIT)
+# @copyright © 2023 Perry Anderson, (perry@exparx.ca)
+# @ref https://github.com/perriera
+#
+# Permission  is  hereby  granted, free  of  charge, to  any person  obtaining
+# a copy of this software and associated documentation files (the “Software”),
+# to deal in the Software  without restriction,  including  without limitation
+# the rights to  use,  copy,  modify,  merge, publish, distribute, sublicense,
+# and/or sell  copies of the  Software,  and  to  permit  persons  to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice  and this permission  notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS  PROVIDED “AS IS”,  WITHOUT WARRANTY  OF ANY  KIND,  EXPRESS
+# OR IMPLIED,  INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR  PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR  COPYRIGHT HOLDERS BE  LIABLE FOR ANY  CLAIM,  DAMAGES  OR  OTHER
+# LIABILITY,  WHETHER IN  AN ACTION  OF CONTRACT,  TORT OR  OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+#
+
+if [ $# -lt 2 ]; 
+  then
+    echo "No arguments supplied"
+    echo Syntax: $0 existing_interface new_interface
+    exit
+fi
+
+if [[ "$1" == "$2" ]]
+  then
+    echo "Cannot copy a folder onto itself"
+    echo Syntax: $0 existing_interface new_interface
+    exit
+fi
+
+PARENT="$(basename "$(dirname "$PWD/test")")"
+echo $PARENT
+
+#
+# duplicate 'include/' folders 
+#
+
+BASE=include/${PARENT}
+echo $BASE
+
+SOURCE=${BASE}/$1
+TARGET=${BASE}/$2
+
+if [ ! -d "${TARGET}" ] 
+then
+    mkdir ${TARGET}
+fi
+
+cp ${SOURCE}/clazz.hpp ${TARGET}/clazz.hpp
+
+sed -i -e "s/${1}/${2}/g" ${TARGET}/clazz.hpp 
+sed -i -e "s/${1^}/${2^}/g" ${TARGET}/clazz.hpp 
+sed -i -e "s/${1^^}/${2^^}/g" ${TARGET}/clazz.hpp 
+
+#
+# duplicate 'interfaces/' folders 
+#
+
+SOURCE=interfaces/$1
+TARGET=interfaces/$2
+
+echo ${SOURCE}
+echo ${TARGET}
+
+if [ ! -d "${TARGET}" ] 
+then
+    mkdir ${TARGET}
+fi
+
+cp ${SOURCE}/dock_instance.cpp ${TARGET}/dock_instance.cpp
+
+sed -i -e "s/${1}/${2}/g" ${TARGET}/dock_instance.cpp
+sed -i -e "s/${1^}/${2^}/g" ${TARGET}/dock_instance.cpp
+sed -i -e "s/${1^^}/${2^^}/g" ${TARGET}/dock_instance.cpp
+
